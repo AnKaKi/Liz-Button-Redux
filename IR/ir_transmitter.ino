42e4b64ba53039c1d33a0bad54186ade0389cbaf
Andrew Yang <andrewyang89@outlook.com>
2020-04-07 08:13:59 -0400

    Adds on/off functionality for Sony TVs

--- IR/ir_transmitter.ino
+++ IR/ir_transmitter.ino
@@ -5,7 +5,7 @@ IRrecv irrecv(RECV_PIN);
 IRsend irsend; // IRSEND pin on my board is 3
 decode_results results;
 
-void setup(){
+ void setup(){
   Serial.begin(115200);
   irrecv.enableIRIn();
   irrecv.blink13(true);
@@ -13,9 +13,25 @@ void setup(){
 
 void loop(){
   // Can read the code of IR input
-  if (irrecv.decode(&results)){
-        Serial.println(results.decode_type);
-        Serial.println(results.value, HEX);
-        irrecv.resume();
-  }
+  // if (irrecv.decode(&results)) {
+  //   if (results.decode_type == NEC) {
+  //     Serial.print("NEC: ");
+  //   } else if (results.decode_type == SONY) {
+  //     Serial.print("SONY: ");
+  //   } else if (results.decode_type == RC5) {
+  //     Serial.print("RC5: ");
+  //   } else if (results.decode_type == RC6) {
+  //     Serial.print("RC6: ");
+  //   } else if (results.decode_type == UNKNOWN) {
+  //     Serial.print("UNKNOWN: ");
+  //   }
+  //   Serial.println(results.value, HEX);
+  //   Serial.println(results.value);
+  //   irrecv.resume(); // Receive the next value
+  // }
+   for (int i = 0; i < 3; i++) {
+      irsend.sendSony(0xa90, 12); // Sony TV power code
+      delay(40);
+    }
+    delay(1000);
 }
