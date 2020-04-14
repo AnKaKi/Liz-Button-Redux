87c2d2c3bf40d0e0e2dcceba8e5dcae4a1e9f471
KimS0221 <62603307+KimS0221@users.noreply.github.com>
2020-04-14 12:06:03 -0400

    added a file for buzzer code

---
+++ buzzerForRemote.ino
@@ -0,0 +1,19 @@
+//For set up, plug the I/O pin on the buzzer into any number pin on the arduino and ground to ground
+int soundPin = 3; //Pin that buzzer I/O is attached to 
+
+void setup() {
+  pinMode(soundPin, OUTPUT); //setting the predefined pin as the output for the buzzer
+  beep(50);
+  beep(50);
+}
+
+void loop() {
+  
+}
+
+void beep(unsigned char delayMs){
+  analogWrite(soundPin, 20); //Sets pin output to high (buzzer on)
+  delay(delayMs); //The ammount of time the buzzer will sound. For the example it is 50 milliseconds
+  analogWrite(soundPin, 0); //Sets pin output to low (buzzer off)
+  delay(delayMs); // The ammount of time the buzzer will not sound. For this example it is the same amount of time as it is on
+}
